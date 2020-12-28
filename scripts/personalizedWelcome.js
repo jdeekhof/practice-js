ee0b7255c0a8794976ca9de8fe14e1592c02bd41
Jake Eekhof <jake@Jakes-MacBook-Air.local>
2020-12-28 14:26:52 -0800

    Added a button to collect name and then render it using the localstorage api

---
+++ scripts/personalizedWelcome.js
@@ -0,0 +1,19 @@
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName(){
+	let myName = prompt('Please Enter your name');
+	localStorage.setItem('name', myName);
+	myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
